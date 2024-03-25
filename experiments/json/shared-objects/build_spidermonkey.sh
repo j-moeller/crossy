aaf2177930760f5f239260e708210deee9630101
Jonas Möller <jonas.moeller.1@tu-berlin.de>
2024-03-25 17:14:13 +0100

    Update

--- experiments/json/shared-objects/build_spidermonkey.sh
+++ experiments/json/shared-objects/build_spidermonkey.sh
@@ -2,8 +2,8 @@
 set -e
 
 export MOZCONFIG=/app/experiments/json/shared-objects/MOZCONFIG_RELEASE
-cd /build/spidermonkey_readonly
-./mach build
+# cd /build/spidermonkey_readonly
+# ./mach build
 
 cd /build/spidermonkey
 ./mach build
