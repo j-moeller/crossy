9a3b48a68020925e5ad07d9181edd9dd36268694
Jonas Möller <jonas.moeller.1@tu-berlin.de>
2024-03-25 16:54:18 +0100

    Update

--- scripts/dev/start_dev_container.sh
+++ scripts/dev/start_dev_container.sh
@@ -22,7 +22,8 @@ docker run -d -it \
     -v $(pwd)/experiments/json/shared-objects/build:/app/experiments/json/shared-objects/build \
     -v $(pwd)/experiments/json/shared-objects/libs:/app/experiments/json/shared-objects/libs \
     -v $(pwd)/experiments/json/shared-objects/libs/spidermonkey:/app/experiments/json/shared-objects/libs/spidermonkey:ro \
-    -v $(pwd)/experiments/json/shared-objects/libs/spidermonkey:/app/spidermonkey_readonly:ro \
+    -v $(pwd)/experiments/json/shared-objects/libs/spidermonkey:/build/spidermonkey_readonly:ro \
+    -v $(pwd)/experiments/json/shared-objects/libs/spidermonkey:/build/spidermonkey \
     \
     -v $(pwd)/src/java:/app/src/java \
     -v $(pwd)/build:/app/build \
